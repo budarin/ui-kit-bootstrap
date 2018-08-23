e756f81e07496c03cb59e69237a4c5c18c363b52
budarin <budarin-vv@yandex.ru>
2018-08-23 18:16:02 +0300

    first commit

---
+++ packages/simple-button/lib/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SimpleText from '@budarin/simple-text';
+
+class SimpleButton extends React.Component {
+  render() {
+    return (
+      <button>
+          <SimpleText>button</SimpleText>
+      </button>
+    );
+  }
+}
+
+export default SimpleButton;
---
+++ packages/simple-text/lib/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SimpleText extends React.Component {
+  render(props) {
+    const { children } = props;
+
+    return (
+      <span>
+          {children}
+      </span>
+    );
+  }
+}
+
+export default SimpleText;
