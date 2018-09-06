a3ca5a1382b768c25553d42132d2d6f00f298343
vadim_budarin <budarin-vv@yandex.ru>
2018-09-06 20:38:42 +0300

    uncomment test

--- packages/simple-button/__tests__/iindex.test.tsx
+++ packages/simple-button/__tests__/iindex.test.tsx
@@ -3,15 +3,15 @@ import { shallow } from 'enzyme';
 import Button from '../src/index';
 
 test('Test Button component', () => {
-    // const btn = shallow(<Button text="Off" />);
+    const btn = shallow(<Button text="Off" />);
 
     // Interaction demo
-    // expect(btn.text()).toEqual('Off');
+    expect(btn.text()).toEqual('Off');
     // btn.find('input').simulate('change');
     // expect(btn.text()).toEqual('On');
 
     // Snapshot demo
-    // expect(btn).toMatchSnapshot();
+    expect(btn).toMatchSnapshot();
 
     expect(0).toBe(0);
 });
