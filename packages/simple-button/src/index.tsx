04d87ea313d337b21543ab8cfd5f1be7aa8adff0
vadim_budarin <budarin-vv@yandex.ru>
2018-09-06 12:12:56 +0300

    add styles to button

--- packages/simple-button/src/index.tsx
+++ packages/simple-button/src/index.tsx
@@ -1,16 +1,29 @@
 import * as React from 'react';
 import SimpleText from '@budarin/simple-text';
 
+// tslint:disable-next-line
+const styles = require('./styles.css');
+
 interface IProps {
     text: string;
 }
 
+const css = window ? styles.locals : styles;
+
 class SimpleButton extends React.Component<IProps> {
+    componentDidMount() {
+        styles.use();
+    }
+
+    componentWillUnmount() {
+        styles.unuse();
+    }
+
     render() {
         const { text = 'Simple button' }: IProps = this.props;
 
         return (
-            <button>
+            <button className={css.btn}>
                 <SimpleText>{text}</SimpleText>
             </button>
         );
