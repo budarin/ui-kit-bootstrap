fdb4359cba83f74010c819eded98e21cad72e9f5
vadim_budarin <budarin-vv@yandex.ru>
2018-09-07 22:16:37 +0300

    update

--- packages/simple-text/__tests__/iindex.test.tsx
+++ packages/simple-text/__tests__/iindex.test.tsx
@@ -3,10 +3,9 @@ import renderer from 'react-test-renderer';
 import SimpleText from '../src/index';
 
 test('0 should be 0', () => {
-    const component = renderer.create(<SimpleText>'Text'</SimpleText>);
+    const component = renderer.create(<SimpleText>Text</SimpleText>);
     const tree = component.toJSON();
 
     expect(tree).toMatchSnapshot();
-
     expect(0).toBe(0);
 });
