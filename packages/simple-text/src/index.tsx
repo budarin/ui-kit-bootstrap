ae7e0535b8699567735c666782d2fb876e9d6272
vadim_budarin <budarin-vv@yandex.ru>
2018-09-28 02:59:50 +0300

    convert SimpleText to SFC

--- packages/simple-text/src/index.tsx
+++ packages/simple-text/src/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 
-class SimpleText extends React.Component {
-    render() {
-        const { children } = this.props;
-        const styles = { fontStyle: 'italic' };
-
-        return <span style={styles}>{children}</span>;
-    }
+interface ISimpleTextProps {
+    children?: any;
 }
+
+const SimpleText: React.SFC<ISimpleTextProps> = props => {
+    const { children } = props;
+    const styles = { fontStyle: 'italic' };
+
+    return <span style={styles}>{children}</span>;
+};
 
 export default SimpleText;
