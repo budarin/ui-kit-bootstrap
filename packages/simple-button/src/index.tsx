78c5ec88c9dc5f2f452dd829c1a3f45c065876c7
vadim_budarin <budarin-vv@yandex.ru>
2018-09-29 02:07:27 +0300

    refactor: set React.HTMLProps<HTMLInputElement> to button props

--- packages/simple-button/src/index.tsx
+++ packages/simple-button/src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import SimpleText from 'budarin-simple-text';
 
-interface IProps {
+interface IProps extends React.HTMLProps<HTMLInputElement> {
     text?: string;
 }
 
